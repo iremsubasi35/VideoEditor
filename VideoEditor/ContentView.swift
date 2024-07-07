04fc1982dd8af40da8d5c3fc1da78e9d2ba86e7d
iremsubasi35 <85845745+iremsubasi35@users.noreply.github.com>
2024-07-08 02:17:34 +0300

    Initial Commit

---
+++ VideoEditor/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  VideoEditor
+//
+//  Created by İrem Subaşı on 8.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VideoEditor/VideoEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoEditorApp.swift
+//  VideoEditor
+//
+//  Created by İrem Subaşı on 8.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct VideoEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
